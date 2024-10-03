522361aa7969eb68ee1567dae4fa80b0b6a3a783
lucas-s-p <lucas.pereira@ccc.ufcg.edu.br>
2024-10-03 20:25:59 -0300

    add: adicionando mais um cliente para teste.

--- src/main/java/br/edu/ufcg/ccc/Main.java
+++ src/main/java/br/edu/ufcg/ccc/Main.java
@@ -10,30 +10,38 @@ import br.edu.ufcg.ccc.system.Produto;
 
 public class Main {
     public static void main(String[] args) {
-        //PRODUTOS
+        // PRODUTOS
         Produto produto = new Produto("Feijão", 13);
         Produto produto_1 = new Produto("Arroz", 5);
+        Produto produto_2 = new Produto("Macarrão", 10);
         
         // SISTEMA
         ECommece eCommece = new ECommece();
 
-        //ESTOQUE
+        // ESTOQUE
         eCommece.adicionarProdutoEstoque(produto, 5);
         eCommece.adicionarProdutoEstoque(produto_1, 8);
+        eCommece.adicionarProdutoEstoque(produto_2, 3);
 
         // CLIENTES
-        Client clientOne = new  Client(eCommece, "A");
-        Client clienttwo = new  Client(eCommece, "B");
+        Client clientOne = new Client(eCommece, "A");
+        Client clienttwo = new Client(eCommece, "B");
+        Client clientThree = new Client(eCommece, "C");
 
-        //SIMULA COMPRA DO CLIENTE ONE
+        // SIMULA COMPRA DO CLIENTE ONE
         clientOne.adicionaProduto(produto, 10);
         clientOne.adicionaProduto(produto_1, 4);
-        ScheduledExecutorService client = Executors.newScheduledThreadPool(1);
-        client.scheduleAtFixedRate(clientOne, 0, 5000, TimeUnit.SECONDS);
+        ScheduledExecutorService client = Executors.newScheduledThreadPool(8);
+        client.scheduleAtFixedRate(clientOne, 0, 150, TimeUnit.SECONDS);
 
-        //SIMULA COMPRA DO CLIENTE TWO
+        // SIMULA COMPRA DO CLIENTE TWO
         clienttwo.adicionaProduto(produto, 10);
         clienttwo.adicionaProduto(produto_1, 4);
-        client.scheduleAtFixedRate(clienttwo, 0, 15, TimeUnit.SECONDS);
+        client.scheduleAtFixedRate(clienttwo, 0, 150, TimeUnit.SECONDS);
+
+        // SIMULA COMPRA DO CLIENTE THREE
+        clientThree.adicionaProduto(produto, 4);
+        clientThree.adicionaProduto(produto_2, 15);
+        client.scheduleAtFixedRate(clientThree, 0, 150, TimeUnit.SECONDS);
     }
 }
